dd9c5ae79ad670d18f0a615531fe8dc325026d06
lzamir <lzamir.lior@gmail.com>
2017-11-16 23:24:03 +0200

    Add searchBar, body and Anchors to the Bubble

--- bubble.js
+++ bubble.js
@@ -5,24 +5,39 @@ document.body.appendChild(bubbleDOM);
 
 var topSearchBar = document.createElement('input');
 topSearchBar.setAttribute("type", "text");
-topSearchBar.classList.add('top_search_bar');
+topSearchBar.setAttribute("placeholder", "Search");
+topSearchBar.classList.add('bubbleTopSearchBar');
 bubbleDOM.appendChild(topSearchBar);
+
+var bubbleBody = document.createElement('div');
+bubbleBody.classList.add('bubbleBody');
+bubbleDOM.appendChild(bubbleBody);
+
+var anchorsBar = document.createElement('div');
+anchorsBar.classList.add('bubbleAnchors');
+bubbleDOM.appendChild(anchorsBar);
+
 
 var selection = '';
 
-function gText(e) {
+function showBubble(e) {
     selection = (document.all) ? document.selection.createRange().text : document.getSelection();
     renderBubble(e.clientX, e.clientY, selection);
 }
+function resetBubble(e) {
+    topSearchBar.value = '';
+}
 
-document.onmouseup = gText;
+document.onmousedown = resetBubble;
+document.onmouseup = showBubble;
+
 if (!document.all) {
     document.captureEvents(Event.MOUSEUP);
 }
 
 // Move that bubble to the appropriate location.
 function renderBubble(mouseX, mouseY, selection) {
-    bubbleDOM.innerHTML = selection;
+    bubbleBody.innerHTML = selection;
     bubbleDOM.style.top = mouseY + 'px';
     bubbleDOM.style.left = mouseX + 'px';
     bubbleDOM.style.display = 'block';
