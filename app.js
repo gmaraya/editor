872349be2df25eaa34947d39001481bc8331f3f0
lzamir <lzamir.lior@gmail.com>
2017-11-17 06:36:55 +0200

    Add events for the navigation

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 ﻿jQuery(function ($) {
     $(".mdl-button.subject").click(function () {
-        $("#addSubject").css("display","block");
+        $("#addSubject").css("display", "block");
     });
 
     $(".mdl-button.structure").click(function () {
         $("#addStructure").css("display", "block");
+    });
+
+    $("nav.mdl-navigation > a").click(function (e) {
+        e.preventDefault();
+        let txt = e.currentTarget.attributes['data-name'].value;
+
+        let gmarayaObj = {};
+
+        GmarayaApi.LoadText(txt).then(data => {
+            gmarayaObj = GmarayaApi.CreateGmarayaObj(data, 'he');
+
+            const selector = document.querySelector('.load-text');
+            selector.innerHTML = gmarayaObj.text.join(' ');
+
+            //GmarayaApi.RunderMarkers(selector, savedMarkers);
+            $(".mdl-layout__drawer").removeClass("is-visible");
+
+        });
     });
 });
